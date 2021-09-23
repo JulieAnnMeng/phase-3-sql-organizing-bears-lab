216ec43c789104ed16602f3623df8b8758b7d942
Julie Meng <julieannmeng@gmail.com>
2021-09-23 13:48:27 -0400

    commit tree cf375d21b73ade2af6f003540e29cfab640215ec

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'M', 'Brown', 'Aggressive', 0),
+('Rowdy', 5, 'M', 'Brown', 'Tame', 1),
+('Tabitha', 4, 'F', 'Brown', 'Tame', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Tame', 1),
+('Melissa', 10, 'F', 'Black', 'Tame', 1),
+('Grinch', 18, 'M', 'Brown', 'Aggressive', 1),
+('Wendy', 8, 'F', 'Brown', 'Aggressive', 1),
+(NULL, 1, 'M', 'Brown', 'Tame', 0);
